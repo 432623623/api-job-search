bb3df147407423132296aba3a0d85a3f8867a6e6
Pat Song <songjh2005@gmail.com>
2024-04-12 18:49:28 -0600

    second commit

--- index.js
+++ index.js
@@ -15,18 +15,31 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine','ejs');
 
 app.get('/', async (req,res)=>{
-  res.render('index.ejs',{result:[], searchTerm:''});
+  let totalPages;
+  res.render('index.ejs',{
+  result:[], 
+  searchTerm:'',
+  page:page,
+  total:0,
+  totalPages:0
+  });
 });
 
-app.post('/', async (req,res)=>{
+app.post('/results/page/1', async (req,res)=>{
   searchTerm = req.body.search.trim();
   if (searchTerm.length > 0){
     const URL = buildSearchURL(searchTerm, page);
     console.log('url:',URL);
     try{  
-      let result = await axios.get(URL);
+      let result = await axios.get(URL);      
+      let totalResults = result.data.count;
+      let totalPages = Math.ceil(totalResults/20);
       res.render('index.ejs',{
-        result: result.data.results, searchTerm:searchTerm
+        result: result.data.results, 
+        searchTerm:searchTerm,
+        page:page,
+        total: totalResults,
+        totalPages: totalPages
       }); 
     }catch(err){
       console.error('error:',err.message);
@@ -37,20 +50,27 @@ app.post('/', async (req,res)=>{
   }
 });
 
-app.get('/next-20',async(req,res)=>{
-  page += 1;
-  const URL = buildSearchURL(searchTerm, page);
-  console.log('url:',URL,' ,page:',page);  
-  if(!isNaN(page)&&page>=0){
-  try{  
-    let result = await axios.get(URL);
-    res.render('index.ejs',{
-      result: result.data.results,searchTerm:searchTerm
-    }); 
-  }catch(err){
+app.get('/results/page/:pageNumber',async(req,res)=>{
+  let nextPage = parseInt(req.params.pageNumber) || 1;
+  console.log('page:', nextPage);
+  if(nextPage > 0){    
+    const URL = buildSearchURL(searchTerm, nextPage);
+    try{  
+      let result = await axios.get(URL);
+      let totalResults = result.data.count;
+      let totalPages = Math.ceil(totalResults/20);
+      res.render('index.ejs',{
+        result: result.data.results,
+        searchTerm:searchTerm,
+        page:nextPage,
+        total: totalResults,
+        totalPages: totalPages
+      }); 
+    }catch(err){
       console.error('error:',err.message);
       res.status(500).send('internal server error');
-  }
+    }  
+  
   }else{
     res.status(400).send('invalid page param');
   }
